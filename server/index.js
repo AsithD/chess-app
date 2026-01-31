b7e77a835d05223d43945fa54bccbf9516149c7b
AsithDasanayake <100549849+AsithDasanayake@users.noreply.github.com>
2026-02-01 10:39:22 +1100

    Fix Express 5 routing syntax for catch-all

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => {
 });
 
 // For any other request, serve the index.html from the dist folder
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
 
